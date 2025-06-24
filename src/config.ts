7157bfbca7679d1ddd6431aa2385bdbdc99b7365
Raun <101993257+Raun0129@users.noreply.github.com>
2025-06-24 18:55:14 +0900

    Can't import the named export 'html'.'flat' -> concat

--- src/config.ts
+++ src/config.ts
@@ -13,7 +13,7 @@ import {html} from 'wikiparser-node/config/default.json';
  *
  * @see https://www.mediawiki.org/wiki/Extension:CodeMirror#Extension_integration
  */
-export const htmlTags = html.flat(),
+export const htmlTags = ([]).concat(...html),
 
 	/** HTML tags that are only self-closing. */
 	voidHtmlTags = html[2]!,
